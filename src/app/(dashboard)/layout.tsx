334a1bcbe85bf9482cc2c84fb0f9923bd928d7c1
Kada Guetouache <wail@disroot.org>
2024-02-27 12:38:20 +0100

    Project basic setup
    add the following features
    - basic project setup with shadcn-ui
    - authentication and authorization with clerk
    - add postgres database pool with prisma ORM

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from "react";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+import { UserButton } from "@clerk/nextjs";
+import Logo from "@/components/Logo";
+
+function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+        <Logo />
+        <div className="flex gap-4 items-center">
+          <ThemeSwitcher />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
